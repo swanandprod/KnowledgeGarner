a1dd368897cc766b5f30a8831a065cf5171177a2
swanandprod <swanandmarulkar84@gmail.com>
2025-02-17 00:42:00 +0100

    Title and Sub header change

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,14 +15,13 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and Open-Source Next.js Template for Startup & SaaS
+                Free KnowledgeHub for Product Managers
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Startup is free Next.js template for startups and SaaS business websites comes with all the essential pages, components, and sections you need to launch a complete business website, built-with Next 13.x and Tailwind CSS.
-                </p>
-                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                A free, centralized platform offering curated resources, tools, and community-driven insights to empower product managers with actionable strategies, frameworks, and industry best practices. Designed to streamline learning and collaboration for professionals at all career stages                </p>
+                {/* <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/startup"
+                    href="https://www.knowledgegarner.com/"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Download Now
@@ -33,7 +32,7 @@ const Hero = () => {
                   >
                     Star on GitHub
                   </Link>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
