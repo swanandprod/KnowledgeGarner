f0284fd71300868f7f62197fb2d04ea0e0a10847
swanandprod <swanandmarulkar84@gmail.com>
2025-02-16 23:31:01 +0100

    Add static export

---
+++ out/_next/static/chunks/app/signin/page-7b15d7999bb48ba3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[217,527,879,977],{6903:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,441,517,358],()=>s(6903)),_N_E=e.O()}]);
